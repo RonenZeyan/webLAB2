d9ab67f32f30aa38162ef452aba4d6d9a7d931db
RonenZeyan <101258047+RonenZeyan@users.noreply.github.com>
2024-01-27 16:43:54 +0200

    third commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let previousNum = 0
 let nextNum = 0
 monitor = document.getElementById('monitor')
 
-
+// method for add char to the display/monitor
 function addCharToMonitor(x){
  monitor.innerHTML += x
  if(x=='pi'){
@@ -38,6 +38,11 @@ function addCharToMonitor(x){
  else if(x=='abs')
  {
     result = Math.abs(result)
+    calculateResult()
+ }
+ else if(x=='exp')
+ {
+    result = Math.exp(result)
     calculateResult()
  }
  else if(x=='fact')
@@ -72,8 +77,12 @@ function addCharToMonitor(x){
  }
 }
 
+// method for buttons not implemented yet 
+function Not_implemented(){
+   alert('not implemented yet!!!')
+}
 
-
+// method for clear the display/monitor
 function clearMonitor(){
     monitor.innerHTML= ''
     lastResult = 0
@@ -81,19 +90,21 @@ function clearMonitor(){
     monitorSTR = ''
 }
 
+// method for delete char
 function delete_Last_char(){
     monitorSTR = monitor.innerHTML
     monitorSTR = monitorSTR.substring(0,monitorSTR.length-1)
     monitor.innerHTML = monitorSTR
 }
 
+// display the result in the monitor
 function calculateResult(){
     if(flag=='mod')
     {
         flag=''
         let start
         start = result.indexOf('mod')
-        nextNum=result.substring(0,start)
+        nextNum=result.substring(0,start)  //get the num before the mod 
         previousNum=result.slice(start+3,result.length)
         result = nextNum%previousNum
         previousNum=0
@@ -105,7 +116,7 @@ function calculateResult(){
         let start
         start = result.indexOf('^')
         nextNum=result.substring(0,start)
-        previousNum=result.slice(start+1,result.length)
+        previousNum=result.slice(start+1,result.length) 
         result = Math.pow(nextNum,previousNum)
         previousNum=0
         nextNum=0
@@ -114,7 +125,7 @@ function calculateResult(){
     try{
     lastResult = eval(result)
     result=lastResult
-    }catch(error){
+    }catch(error){ 
       result='Syntax Error'
       lastResult='Syntax Error'
     }
